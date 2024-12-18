867f63129033c1f5aa5290442782a2e7aba49a3a
Viktor Lidholt <viktorlidholt@gmail.com>
2024-12-18 14:33:06 +0100

    docs: Updates documentations and adds example.

---
+++ example/example.dart
@@ -0,0 +1,21 @@
+import 'package:relic/relic.dart';
+
+void main() async {
+  var handler =
+      const Pipeline().addMiddleware(logRequests()).addHandler(_echoRequest);
+
+  var server = await serve(handler, 'localhost', 8080);
+
+  // Enable content compression
+  server.autoCompress = true;
+
+  print('Serving at http://${server.address.host}:${server.port}');
+}
+
+Response _echoRequest(Request request) {
+  return Response.ok(
+    body: Body.fromString(
+      'Request for "${request.url}"',
+    ),
+  );
+}
