d64513022452afac349df858e1d9d14fe704abd8
Klaudio Kuçaj <kkucaj.contact@gmail.com>
2025-01-02 11:04:49 +0100

    chore: add lints and enforce stricter typing and future handling

--- test/relic_server_serve_test.dart
+++ test/relic_server_serve_test.dart
@@ -126,6 +126,7 @@ void main() {
       Uri.http('localhost:$_serverPort', '/'),
     );
     request.sink.add([1, 2, 3, 4]);
+    // ignore: unawaited_futures
     request.sink.close();
 
     var response = await request.send();
