de69af2495eef4264a57ae134b88ca9d96a6dc83
Kasper Overgård Nielsen <kasper@byolimit.com>
2025-03-05 08:37:32 +0100

    test:Use expando to embellish RelicServer with a url to use for testing (avoids a larger test refactoring)

--- test/headers/headers_test_utils.dart
+++ test/headers/headers_test_utils.dart
@@ -13,6 +13,39 @@ class BadRequestException implements Exception {
   );
 }
 
+extension RelicServerTestEx on RelicServer {
+  static final Expando<Uri> _serverUrls = Expando();
+
+  /// Fake [url] property for the [RelicServer] for testing purposes.
+  Uri get url => _serverUrls[this] ??= _inferUrl();
+  set url(Uri value) => _serverUrls[this] = value;
+
+  /// Infer a probable URL for the server.
+  ///
+  /// In general a server cannot know what URL it is being accessed by before an
+  /// actual request arrives, but for testing purposes we can infer a URL based
+  /// on the server's address.
+  Uri _inferUrl() {
+    if (server.address.isLoopback) {
+      return Uri(scheme: 'http', host: 'localhost', port: server.port);
+    }
+
+    if (server.address.type == InternetAddressType.IPv6) {
+      return Uri(
+        scheme: 'http',
+        host: '[${server.address.address}]',
+        port: server.port,
+      );
+    }
+
+    return Uri(
+      scheme: 'http',
+      host: server.address.address,
+      port: server.port,
+    );
+  }
+}
+
 /// Creates a [RelicServer] that listens on the loopback IPv6 address.
 /// If the IPv6 address is not available, it will listen on the loopback IPv4
 /// address.
@@ -20,13 +53,13 @@ Future<RelicServer> createServer({
   required bool strictHeaders,
 }) async {
   try {
-    return RelicServer.createServer(
+    return await RelicServer.createServer(
       InternetAddress.loopbackIPv6,
       0,
       strictHeaders: strictHeaders,
     );
   } on SocketException catch (_) {
-    return RelicServer.createServer(
+    return await RelicServer.createServer(
       InternetAddress.loopbackIPv4,
       0,
       strictHeaders: strictHeaders,
@@ -49,7 +82,7 @@ Future<Headers> getServerRequestHeaders({
       parsedHeaders = request.headers;
 
       if (parseAllHeaders) {
-        parsedHeaders?.toMap();
+        parsedHeaders?.toMap(); // parse all headers eagerly (if any)
       }
 
       return Response.ok();
