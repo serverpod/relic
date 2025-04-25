a430d90741d5b626646ad2165d293050d43f77a6
Kasper Overgård Nielsen <kasper@byolimit.com>
2025-04-25 14:53:04 +0200

    Tweak example (doc-) comments

--- example/example.dart
+++ example/example.dart
@@ -2,24 +2,20 @@ import 'dart:io';
 
 import 'package:relic/relic.dart';
 
+/// A simple 'Hello World' server
 Future<void> main() async {
-  /// Setup a handler.
-  ///
-  /// A [Handler] is function consuming and producing [RequestContext]s,
-  /// but if you are mostly concerned with converting [Request]s to [Response]s
-  /// (known as a [Responder] in relic parlor) you can use [respondWith] to
-  /// wrap a [Responder] into a [Handler]
+  // Setup a handler.
+  //
+  // A [Handler] is function consuming and producing [RequestContext]s,
+  // but if you are mostly concerned with converting [Request]s to [Response]s
+  // (known as a [Responder] in relic parlor) you can use [respondWith] to
+  // wrap a [Responder] into a [Handler]
   final handler = respondWith(hello);
 
-  /// Start the server with the handler
-  ///
-  /// [serve] is just short-hand for
-  /// ```dart
-  /// RelicServer(IOAdator(HttpServer.bind(InternetAddress.anyIPv4, 8080)))
-  /// ```
+  // Start the server with the handler
   await serve(handler, InternetAddress.anyIPv4, 8080);
 
-  /// Check the _example_ directory for other examples.
+  // Check the _example_ directory for other examples.
 }
 
 /// A very simple [Responder].
--- example/multi_isolate.dart
+++ example/multi_isolate.dart
@@ -5,29 +5,39 @@ import 'dart:isolate';
 import 'package:relic/relic.dart';
 
 void main() async {
+  // The number of isolates to use. Making it proportional to number of
+  // processors ensure we have enough isolates to utilize the hardware.
   final isolateCount = Platform.numberOfProcessors * 2;
 
+  // Wait for all the isolates to spawn
   print('Starting $isolateCount isolates');
   final isolates = await Future.wait(List.generate(
       isolateCount,
       (final index) =>
           Isolate.spawn((final _) => _serve(), null, debugName: '$index')));
 
+  // Wait for Ctrl-C before proceeding
   await ProcessSignal.sigint.watch().first;
 
+  // Shutdown again.
   for (final i in isolates) {
     i.kill(priority: Isolate.immediate);
   }
 }
 
+/// [_serve] is called in each spawned isolate.
 Future<void> _serve() async {
+  ///
   final handler = const Pipeline()
-      .addMiddleware(logRequests())
-      .addHandler(respondWith(_echoRequest));
+      .addMiddleware(logRequests()) // setup logging
+      .addHandler(respondWith(_echoRequest)); // add our handler
+
+  // start the server
   await serve(handler, InternetAddress.anyIPv4, 8080, shared: true);
   print('serving on ${Isolate.current.debugName}');
 }
 
+/// [_echoRequest] just echoes the path of the request
 Response _echoRequest(final Request request) {
   sleep(const Duration(seconds: 1)); // pretend to be really slow
   return Response.ok(
