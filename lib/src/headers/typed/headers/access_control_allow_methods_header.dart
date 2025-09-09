5d228b2681726bea59c19b156b47ea329fb10cf3
Kasper Overgård Nielsen <kasper@byolimit.com>
2025-09-09 11:03:16 +0200

    refactor: Make AccessControlAllowMethodsHeader extend WildcardListHeader
    - Convert AccessControlAllowMethodsHeader to extend WildcardListHeader<RequestMethod>
    - Use WildcardListHeader.parse for consistent parsing logic
    - Remove duplicated wildcard handling code
    - Follow same pattern as AcceptEncodingHeader and AcceptLanguageHeader

--- lib/src/headers/typed/headers/access_control_allow_methods_header.dart
+++ lib/src/headers/typed/headers/access_control_allow_methods_header.dart
@@ -1,79 +1,47 @@
-import 'package:collection/collection.dart';
-
 import '../../../../relic.dart';
-import '../../extension/string_list_extensions.dart';
+import 'wildcard_list_header.dart';
 
 /// A class representing the HTTP Access-Control-Allow-Methods header.
 ///
 /// This header specifies which methods are allowed when accessing the resource
 /// in response to a preflight request.
-final class AccessControlAllowMethodsHeader {
-  static const codec =
-      HeaderCodec(AccessControlAllowMethodsHeader.parse, __encode);
-  static List<String> __encode(final AccessControlAllowMethodsHeader value) =>
-      [value._encode()];
-
-  /// The list of methods that are allowed.
-  final List<RequestMethod> methods;
-
-  /// Whether all methods are allowed (`*`).
-  final bool isWildcard;
+final class AccessControlAllowMethodsHeader
+    extends WildcardListHeader<RequestMethod> {
+  static const codec = HeaderCodec(_parse, _encode);
 
   /// Constructs an instance allowing specific methods to be allowed.
   AccessControlAllowMethodsHeader.methods(
       {required final List<RequestMethod> methods})
-      : assert(methods.isNotEmpty),
-        methods = List.unmodifiable(methods),
-        isWildcard = false;
+      : super(methods);
 
   /// Constructs an instance allowing all methods to be allowed (`*`).
-  const AccessControlAllowMethodsHeader.wildcard()
-      : methods = const [],
-        isWildcard = true;
+  const AccessControlAllowMethodsHeader.wildcard() : super.wildcard();
 
   /// Parses the Access-Control-Allow-Methods header value and returns an
   /// [AccessControlAllowMethodsHeader] instance.
   factory AccessControlAllowMethodsHeader.parse(final Iterable<String> values) {
-    final splitValues = values.splitTrimAndFilterUnique();
-    if (splitValues.isEmpty) {
-      throw const FormatException(
-        'Value cannot be empty',
-      );
-    }
-
-    if (splitValues.length == 1 && splitValues.first == '*') {
-      return const AccessControlAllowMethodsHeader.wildcard();
-    }
-
-    if (splitValues.length > 1 && splitValues.contains('*')) {
-      throw const FormatException(
-        'Wildcard (*) cannot be used with other values',
-      );
-    }
-
-    return AccessControlAllowMethodsHeader.methods(
-      methods: splitValues.map(RequestMethod.parse).toList(),
-    );
+    return _parse(values);
   }
 
-  /// Converts the [AccessControlAllowMethodsHeader] instance into a string
-  /// representation suitable for HTTP headers.
+  /// The list of methods that are allowed
+  List<RequestMethod> get methods => values;
 
-  String _encode() =>
-      isWildcard ? '*' : methods.map((final m) => m.value).join(', ');
+  static AccessControlAllowMethodsHeader _parse(final Iterable<String> values) {
+    final parsed = WildcardListHeader.parse(values, RequestMethod.parse);
 
-  @override
-  bool operator ==(final Object other) =>
-      identical(this, other) ||
-      other is AccessControlAllowMethodsHeader &&
-          isWildcard == other.isWildcard &&
-          const ListEquality<RequestMethod>().equals(methods, other.methods);
+    if (parsed.isWildcard) {
+      return const AccessControlAllowMethodsHeader.wildcard();
+    } else {
+      return AccessControlAllowMethodsHeader.methods(methods: parsed.values);
+    }
+  }
 
-  @override
-  int get hashCode => Object.hash(
-      isWildcard, const ListEquality<RequestMethod>().hash(methods));
+  static List<String> encodeHeader(
+      final AccessControlAllowMethodsHeader header) {
+    return header.encode((final RequestMethod rm) => rm.value).toList();
+  }
 
-  @override
-  String toString() =>
-      'AccessControlAllowMethodsHeader(methods: $methods, isWildcard: $isWildcard)';
+  static List<String> _encode(final AccessControlAllowMethodsHeader header) {
+    return encodeHeader(header);
+  }
 }
