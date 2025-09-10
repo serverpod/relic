c3ebe45683f2bc28c6309b47ec705f07345b728d
Kasper Overgård Nielsen <kasper@byolimit.com>
2025-09-10 13:56:47 +0200

    feat!: Remove automatic X-Powered-By header
    BREAKING CHANGE: The serve() function no longer accepts a poweredByHeader
    parameter and Relic no longer automatically sets the X-Powered-By header
    on responses. Users who want this header can set it manually using
    middleware or by setting it directly on Response objects.
    
    Fixes #165

--- lib/src/adapter/io/io_serve.dart
+++ lib/src/adapter/io/io_serve.dart
@@ -9,36 +9,32 @@ import 'io_adapter.dart';
 /// Starts a server that listens on the specified [address] and
 /// [port] and sends requests to [handler].
 ///
-/// If [security] is provided, a secure server will be started.
+/// If [securityContext] is provided, a secure server will be started.
 ///
 /// {@template relic_server_header_defaults}
-/// Every response will get a "date" header and an "X-Powered-By" header.
-/// If either header is present in the `Response`, it will not be
+/// Every response will get a "date" header.
+/// If this header is present in the `Response`, it will not be
 /// overwritten.
-/// Pass [poweredByHeader] to set the default content for "X-Powered-By",
-/// pass `null` to omit this header.
 /// {@endtemplate}
 Future<RelicServer> serve(
   final Handler handler,
   final InternetAddress address,
   final int port, {
-  final SecurityContext? context,
+  final SecurityContext? securityContext,
   final int? backlog,
   final bool shared = false,
   final bool strictHeaders = false,
-  final String? poweredByHeader = RelicServer.defaultPoweredByHeader,
 }) async {
   final adapter = IOAdapter(await bindHttpServer(
     address,
     port: port,
-    context: context,
+    context: securityContext,
     backlog: backlog ?? 0,
     shared: shared,
   ));
   final server = RelicServer(
     adapter,
     strictHeaders: strictHeaders,
-    poweredByHeader: poweredByHeader,
   );
   await server.mountAndStart(handler);
   return server;
