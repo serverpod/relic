5905a98eedba96f581dbc83a379a70b415a1b3f4
Kasper Overgård Nielsen <kasper@byolimit.com>
2025-09-15 12:32:13 +0200

    test: Add test for dynamic cache control

--- test/static/cache_control_test.dart
+++ test/static/cache_control_test.dart
@@ -11,7 +11,10 @@ void main() {
   late Handler handler;
 
   setUp(() async {
-    await d.file('test_file.txt', fileContent).create();
+    await d.dir('', [
+      d.file('test_file.txt', fileContent),
+      d.file('image.jpg'),
+    ]).create();
     handler = createStaticHandler(cacheControl: (final _) => null, d.sandbox);
   });
 
@@ -45,6 +48,33 @@ void main() {
       'then the response includes no Cache-Control header', () async {
     final response = await makeRequest(handler, '/test_file.txt');
 
+    expect(response.statusCode, HttpStatus.ok);
+    expect(response.headers.cacheControl, isNull);
+  });
+
+  test(
+      'Given Cache-Control header is set on the server for some files '
+      'when such a file is served, '
+      'then the response includes the specified Cache-Control header '
+      'otherwise not', () async {
+    final cacheControl = CacheControlHeader(
+      maxAge: 3600,
+      publicCache: true,
+      mustRevalidate: true,
+    );
+    handler = createStaticHandler(
+      d.sandbox,
+      cacheControl: (final fileInfo) =>
+          fileInfo.file.path.endsWith('jpg') ? cacheControl : null,
+    );
+    var response = await makeRequest(handler, '/image.jpg');
+    expect(response.statusCode, HttpStatus.ok);
+    expect(response.headers.cacheControl, isNotNull);
+    expect(response.headers.cacheControl!.maxAge, 3600);
+    expect(response.headers.cacheControl!.publicCache, isTrue);
+    expect(response.headers.cacheControl!.mustRevalidate, isTrue);
+
+    response = await makeRequest(handler, '/test_file.txt');
     expect(response.statusCode, HttpStatus.ok);
     expect(response.headers.cacheControl, isNull);
   });
