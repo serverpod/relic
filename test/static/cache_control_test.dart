4f70a9936862f6bebe85d747fbf6ffa2487f1038
Kasper Overgård Nielsen <kasper@byolimit.com>
2025-09-18 13:14:16 +0200

    test: Split a test in two

--- test/static/cache_control_test.dart
+++ test/static/cache_control_test.dart
@@ -8,15 +8,17 @@ import 'test_util.dart'; // Provides the makeRequest helper
 
 void main() {
   const fileContent = '0123456789ABCDEF'; // 16 bytes
-  late Handler handler;
+  final cacheControl = CacheControlHeader(
+    maxAge: 3600,
+    publicCache: true,
+    mustRevalidate: true,
+  );
 
-  setUp(() async {
+  setUpAll(() async {
     await d.dir('', [
       d.file('test_file.txt', fileContent),
       d.file('image.jpg'),
     ]).create();
-    handler = createStaticHandler(
-        cacheControl: (final _, final __) => null, d.sandbox);
   });
 
   test(
@@ -24,16 +26,8 @@ void main() {
       'when a file is served, '
       'then the response includes the specified Cache-Control header',
       () async {
-    final cacheControl = CacheControlHeader(
-      maxAge: 3600,
-      publicCache: true,
-      mustRevalidate: true,
-    );
-    handler = createStaticHandler(
-      d.sandbox,
-      cacheControl: (final _, final __) => cacheControl,
-    );
-
+    final handler = createStaticHandler(d.sandbox,
+        cacheControl: (final _, final __) => cacheControl);
     final response = await makeRequest(handler, '/test_file.txt');
 
     expect(response.statusCode, HttpStatus.ok);
@@ -47,36 +41,43 @@ void main() {
       'Given no Cache-Control header is specified on the server, '
       'when a file is served, '
       'then the response includes no Cache-Control header', () async {
+    final handler = createStaticHandler(d.sandbox,
+        cacheControl: (final _, final __) => null);
     final response = await makeRequest(handler, '/test_file.txt');
 
     expect(response.statusCode, HttpStatus.ok);
     expect(response.headers.cacheControl, isNull);
   });
 
-  test(
-      'Given Cache-Control header is set on the server for some files '
-      'when such a file is served, '
-      'then the response includes the specified Cache-Control header '
-      'otherwise not', () async {
-    final cacheControl = CacheControlHeader(
-      maxAge: 3600,
-      publicCache: true,
-      mustRevalidate: true,
-    );
-    handler = createStaticHandler(
-      d.sandbox,
-      cacheControl: (final _, final fileInfo) =>
-          fileInfo.file.path.endsWith('jpg') ? cacheControl : null,
-    );
-    var response = await makeRequest(handler, '/image.jpg');
-    expect(response.statusCode, HttpStatus.ok);
-    expect(response.headers.cacheControl, isNotNull);
-    expect(response.headers.cacheControl!.maxAge, 3600);
-    expect(response.headers.cacheControl!.publicCache, isTrue);
-    expect(response.headers.cacheControl!.mustRevalidate, isTrue);
+  group('Given Cache-Control header is set for a file pattern', () {
+    late Handler handler;
+    setUpAll(() {
+      handler = createStaticHandler(
+        d.sandbox,
+        cacheControl: (final _, final fileInfo) =>
+            fileInfo.file.path.endsWith('jpg') ? cacheControl : null,
+      );
+    });
 
-    response = await makeRequest(handler, '/test_file.txt');
-    expect(response.statusCode, HttpStatus.ok);
-    expect(response.headers.cacheControl, isNull);
+    test(
+        'when a matching file is served, '
+        'then the response includes the specified Cache-Control header',
+        () async {
+      final response = await makeRequest(handler, '/image.jpg');
+      expect(response.statusCode, HttpStatus.ok);
+      expect(response.headers.cacheControl, isNotNull);
+      expect(response.headers.cacheControl!.maxAge, 3600);
+      expect(response.headers.cacheControl!.publicCache, isTrue);
+      expect(response.headers.cacheControl!.mustRevalidate, isTrue);
+    });
+
+    test(
+        'when a non-matching file is served, '
+        "then the response doesn't includes the specified Cache-Control header",
+        () async {
+      final response = await makeRequest(handler, '/test_file.txt');
+      expect(response.statusCode, HttpStatus.ok);
+      expect(response.headers.cacheControl, isNull);
+    });
   });
 }
