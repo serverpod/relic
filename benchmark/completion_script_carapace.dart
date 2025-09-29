54b727b0db0797e8e1e55c016d42d366d41e7794
Kasper Overgård Nielsen <kasper@byolimit.com>
2025-09-29 09:43:41 +0200

    feat: Enable completions for benchmark command

---
+++ benchmark/completion_script_carapace.dart
@@ -0,0 +1,52 @@
+/// This file is auto-generated.
+library;
+
+import 'package:cli_tools/better_command_runner.dart' show CompletionTarget;
+
+const String _completionScript = r'''
+# yaml-language-server: $schema=https://carapace.sh/schemas/command.json
+name: benchmark
+persistentFlags:
+  -q, --quiet: Suppress all cli output. Is overridden by  -v, --verbose.
+  -v, --verbose: Prints additional information useful for development. Overrides --q, --quiet.
+
+commands:
+  - name: completion
+
+    commands:
+      - name: generate
+        flags:
+          -t, --target=!: The target tool format
+          -e, --exec-name=: Override the name of the executable
+          -f, --file=: Write the specification to a file instead of stdout
+        completion:
+          flag:
+            target: ["completely", "carapace"]
+            file: ["$files"]
+
+  - name: run
+    flags:
+      -o, --output=: The file to write benchmark results to
+      -i, --iterations=: Something to do with scale
+      -s, --store-in-git-notes: Store benchmark result with git notes
+      --no-store-in-git-notes: Store benchmark result with git notes
+      -p, --pause-on-startup: Pause on startup to allow devtools to attach
+    exclusiveFlags:
+      - [store-in-git-notes, no-store-in-git-notes]
+    completion:
+      flag:
+        output: ["$files"]
+
+  - name: extract
+    flags:
+      -f, --from=: 
+      -t, --to=: 
+
+
+''';
+
+/// Embedded script for command line completion for `carapace`.
+const completionScriptCarapace = (
+  target: CompletionTarget.carapace,
+  script: _completionScript,
+);
