247ddbb877629a1e48028b47a60b0afe68449dae
Kasper Overgård Nielsen <kasper@byolimit.com>
2025-10-31 15:04:28 +0100

    fix: Hot-reload regression

--- test/router/relic_app_test.dart
+++ test/router/relic_app_test.dart
@@ -88,26 +88,34 @@ void main() {
         return;
       }
 
+      final vmService = await vmi.vmServiceConnectUri(wsUri.toString());
+      final isolateId = Service.getIsolateId(Isolate.current)!;
+      final packagesUri = (await Isolate.packageConfig)?.toString();
+      Future<void> hotReload() async {
+        await vmService.reloadSources(
+          isolateId,
+          force: true,
+          packagesUri: packagesUri,
+        );
+      }
+
       int count = 0;
       final called = StreamController<int>();
-      final app = RelicApp()..inject(_Injectable(() => called.add(++count)));
+      final app =
+          RelicApp()
+            ..inject(_Injectable(() => called.add(++count))); // 1 original
 
       await app.serve();
 
-      final vmService = await vmi.vmServiceConnectUri(wsUri.toString());
+      await hotReload(); // 2
+      await hotReload(); // 3
+      await app.close();
+      await hotReload(); // won't emit
+      await hotReload(); // won't emit
 
-      final isolateId = Service.getIsolateId(Isolate.current)!;
-
-      await vmService.reloadSources(
-        isolateId,
-        force: true,
-        packagesUri: (await Isolate.packageConfig)?.toString(),
-      );
-
-      await expectLater(called.stream, emitsInOrder([1, 2]));
+      await expectLater(called.stream, emitsInOrder([1, 2, 3]));
 
       await vmService.dispose();
-      await app.close();
     });
   });
 }
