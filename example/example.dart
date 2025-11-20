b3e579d61d58d2c8d9a27f0bb471a446fbe16ee6
Viktor Lidholt <viktorlidholt@gmail.com>
2025-11-20 14:24:34 +0100

    fix: Fixes the hello world example.

--- example/example.dart
+++ example/example.dart
@@ -2,33 +2,33 @@
 import 'package:relic/io_adapter.dart';
 import 'package:relic/relic.dart';
 
-/// A simple 'Hello World' server
+/// A simple 'Hello World' server.
 Future<void> main() async {
-  // Setup app
+  // Setup the app.
   final app =
       RelicApp()
-        ..get(
-          '/user/:name/age/:age',
-          hello,
-        ) // route with parameters (:name & :age)
-        ..use('/', logRequests()) // middleware on all paths below '/'
-        // custom fallback - optional (default is 404 Not Found)
+        // Route with parameters (:name & :age).
+        ..get('/user/:name/age/:age', hello)
+        // Middleware on all paths below '/'.
+        ..use('/', logRequests())
+        // Custom fallback - optional (default is 404 Not Found).
         ..fallback = respondWith(
           (_) => Response.notFound(
-            body: Body.fromString("Sorry, that doesn't compute"),
+            body: Body.fromString("Sorry, that doesn't compute.\n"),
           ),
         );
 
-  // Start the server. Defaults to using port 8080 on loopback interface
+  // Start the server (defaults to using port 8080).
   await app.serve();
 }
 
+/// Handles a hello request.
 Response hello(final Request req) {
   final name = req.pathParameters[#name];
   final age = int.parse(req.pathParameters[#age]!);
 
   return Response.ok(
-    body: Body.fromString('Hello $name! To think you are $age years old.'),
+    body: Body.fromString('Hello $name! To think you are $age years old.\n'),
   );
 }
 
