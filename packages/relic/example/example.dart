607fb6e47af800590917ac2d9aabf0a6e3b0963f
Kasper Overgård Nielsen <kasper@byolimit.com>
2025-12-10 10:02:46 +0100

    refactor: Move examples to separate package

--- packages/relic/example/example.dart
+++ packages/relic/example/example.dart
@@ -1,34 +1,13 @@
-// doctag<hello-world-app>
 import 'package:relic/relic.dart';
 
 /// A simple 'Hello World' server demonstrating basic Relic usage.
 Future<void> main() async {
-  // Setup the app.
-  final app =
-      RelicApp()
-        // Route with parameters (:name & :age).
-        ..get('/user/:name/age/:age', helloHandler)
-        // Middleware on all paths below '/'.
-        ..use('/', logRequests())
-        // Custom fallback - optional (default is 404 Not Found).
-        ..fallback = respondWith(
-          (_) => Response.notFound(
-            body: Body.fromString("Sorry, that doesn't compute.\n"),
-          ),
-        );
+  final app = RelicApp()..get('/hello/:name', helloHandler);
 
-  // Start the server (defaults to using port 8080).
   await app.serve();
 }
 
-/// Handles requests to the hello endpoint with path parameters.
 Response helloHandler(final Request req) {
   final name = req.rawPathParameters[#name];
-  final age = int.parse(req.rawPathParameters[#age]!);
-
-  return Response.ok(
-    body: Body.fromString('Hello, $name! To think you are $age years old.\n'),
-  );
+  return Response.ok(body: Body.fromString('Hello, $name!\n'));
 }
-
-// end:doctag<hello-world-app>
