509cc79c58d3f7da7f82b3abf854f87bd106cc4d
Kasper Overgård Nielsen <kasper@byolimit.com>
2026-01-23 15:18:29 +0100

    fix: Remove unused imports

--- packages/relic_io/test/static/alternative_root_test.dart
+++ packages/relic_io/test/static/alternative_root_test.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 
-import 'package:relic_core/relic_core.dart';
 import 'package:relic_io/relic_io.dart';
 import 'package:test/test.dart';
 import 'package:test_descriptor/test_descriptor.dart' as d;
--- packages/relic_io/test/static/symbolic_link_test.dart
+++ packages/relic_io/test/static/symbolic_link_test.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 
 import 'package:path/path.dart' as p;
-import 'package:relic_core/relic_core.dart';
 import 'package:relic_io/relic_io.dart';
 import 'package:test/test.dart';
 import 'package:test_descriptor/test_descriptor.dart' as d;
